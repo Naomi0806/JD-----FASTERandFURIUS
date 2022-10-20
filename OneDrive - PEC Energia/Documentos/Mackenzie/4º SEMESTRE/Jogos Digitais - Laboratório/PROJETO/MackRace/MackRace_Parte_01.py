0c4c3303fb45724bf71aae056e630f0300689a15
gicatelli <catelligiovanna@gmail.com>
2022-10-20 20:48:26 -0300

    changes

--- OneDrive - PEC Energia/Documentos/Mackenzie/4º SEMESTRE/Jogos Digitais - Laboratório/PROJETO/MackRace/MackRace_Parte_01.py
+++ OneDrive - PEC Energia/Documentos/Mackenzie/4º SEMESTRE/Jogos Digitais - Laboratório/PROJETO/MackRace/MackRace_Parte_01.py
@@ -1,10 +1,16 @@
 from pickle import FALSE
 import pygame
+
 pygame.init()
-x = 400
-y = 600
+x = 350
+y = 400
 v = 10
-fundo = pygame.image.load('pista.png')
+y1 = -50
+v1 = -20
+
+fundo = pygame.image.load('pista.jpg')
+pista1 = pygame.image.load('pista3.png')
+pista2 = pygame.image.load('pista4.png')
 carro = pygame.image.load('carro.png')
 janela = pygame.display.set_mode((800,650))
 pygame.display.set_caption('Jogo parte 1')
@@ -15,17 +21,22 @@ while janela_aberta:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_aberta = FALSE
-comandos = pygame .key.get_pressed()
-if comandos [pygame.K_UP]:
-    y- = v
-if comandos [pygame.K_DOWN]:
-    y- = v
-if comandos [pygame.K_RIGHT]:
-    y- = 
-if comandos [pygame.K_LEFT]:
-    y- = 
-    janela.blit(fundo,(0,0))
-    carro.blit(carro(x,y))
+    comandos = pygame.key.get_pressed()
+    if comandos [pygame.K_UP]:
+        y -= v
+    if comandos [pygame.K_DOWN]:
+        y += v
+    if comandos [pygame.K_RIGHT]:
+        y += v
+    if comandos [pygame.K_LEFT]:
+        y -= v
+    y1 -= v1
+    if (y1 >= 700):
+        y1 =- 50
+    janela.blit(fundo, (0,0))
+    janela.blit(pista1, (306, y1))
+    janela.blit(pista2, (496, y1))
+    janela.blit(carro, (x,y))
     pygame.display.update()
 pygame.quit()
 
