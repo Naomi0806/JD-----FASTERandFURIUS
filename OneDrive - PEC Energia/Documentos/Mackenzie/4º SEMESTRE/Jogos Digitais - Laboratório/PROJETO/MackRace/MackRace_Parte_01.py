bd624465d53b733e2743508eed5d3c9b07616478
gicatelli <catelligiovanna@gmail.com>
2022-11-19 18:34:30 -0300

    bugs solved

--- OneDrive - PEC Energia/Documentos/Mackenzie/4º SEMESTRE/Jogos Digitais - Laboratório/PROJETO/MackRace/MackRace_Parte_01.py
+++ OneDrive - PEC Energia/Documentos/Mackenzie/4º SEMESTRE/Jogos Digitais - Laboratório/PROJETO/MackRace/MackRace_Parte_01.py
@@ -1,43 +1,88 @@
 from pickle import FALSE
+from random import randint
 import pygame
-
 pygame.init()
-x = 350
-y = 400
-v = 10
-y1 = -50
-v1 = -20
 
-fundo = pygame.image.load('pista.jpg')
-pista1 = pygame.image.load('pista3.png')
-pista2 = pygame.image.load('pista4.png')
-carro = pygame.image.load('carro.png')
-janela = pygame.display.set_mode((800,650))
+x = 470
+y = 500
+v = 20
+
+v1 = -25
+y1 = -25
+
+v2 = -20
+y2 = -20
+
+v3 = -20
+y3 = -30
+
+v4 = -20
+y4 = -25
+
+timer = 0
+seconds = 0
+
+fundo = pygame.image.load('imagens/estrada.jpeg')
+pista1 = pygame.image.load('imagens/movimento.jpeg')
+pista2 = pygame.image.load('imagens/movimento.jpeg')
+samu = pygame.image.load('imagens/ambulancia.png')
+moto = pygame.image.load('imagens/moto.png')
+carro2 = pygame.image.load('imagens/carro2.png')
+carro = pygame.image.load('imagens/carro.png')
+janela = pygame.display.set_mode((1000,782))
 pygame.display.set_caption('Jogo parte 1')
+
+font = pygame.font.SysFont('arial', 30)
+texto = font.render('Tempo:   ', True, (0, 0, 0), (255,255,0))
+pos_texto = texto.get_rect()
+pos_texto.center = (65, 50)
 
 janela_aberta = True
 while janela_aberta:
     pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            janela_aberta = FALSE
+            janela_aberta = False
+            
     comandos = pygame.key.get_pressed()
-    if comandos [pygame.K_UP]:
-        y -= v
-    if comandos [pygame.K_DOWN]:
-        y += v
-    if comandos [pygame.K_RIGHT]:
-        y += v
-    if comandos [pygame.K_LEFT]:
-        y -= v
+#    if comandos [pygame.K_UP]:
+#        y -= v
+#    if comandos [pygame.K_DOWN]:
+#        y += v
+    if comandos [pygame.K_RIGHT] and x <= 750:
+        x += v
+    if comandos [pygame.K_LEFT] and x >= 155:
+        x -= v
+        
     y1 -= v1
-    if (y1 >= 700):
-        y1 =- 50
+    y2 -= v2
+    y3 -= v3
+    y4 -= v4
+    
+    if (y1 >= 842):
+        y1 =- 25
+    if (y2 >= 884):
+        y2 = randint(-300, -250)
+    if (y3 >= 920):
+        y3 = randint(-100, -50)
+    if (y4 >= 800):
+        y4 = randint(-500, -450)
+        
+    if (timer < 20):
+        timer += 1
+    else:
+        seconds += 1
+        texto = font.render(' Tempo: ' + str(seconds) + " ", True, (0, 0, 0), (255,255,0))
+        timer = 0
+        
     janela.blit(fundo, (0,0))
-    janela.blit(pista1, (306, y1))
-    janela.blit(pista2, (496, y1))
+    janela.blit(pista1, (370, y1))
+    janela.blit(pista2, (610, y1))
+    janela.blit(samu, (655, y4))
+    janela.blit(moto, (470, y3))
+    janela.blit(carro2, (175, y2))
     janela.blit(carro, (x,y))
+    janela.blit(texto, pos_texto)
     pygame.display.update()
+
 pygame.quit()
-
-
